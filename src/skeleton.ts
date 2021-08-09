bce0f71c74ca8743ab52319284863671901e385f
ibrahimabdu <ibrahim.abdu777@gmail.com>
2021-08-09 13:39:10 +0100

    comit

--- src/skeleton.ts
+++ src/skeleton.ts
@@ -5,17 +5,17 @@
  * @returns the total
  */
 function twoNumberSum(array: number[], targetSum: number): number[] {
-  array.sort((a, b) => a - b)
-  let pointer1 = 0
-  let pointer2 = array.length - 1
-  while (pointer1 < pointer2){
-    if (array[pointer1] + array[pointer2] < targetSum) pointer1++
-    else if (array[pointer1] + array[pointer2] < targetSum) pointer2 --
-    else return [array[pointer1], array[pointer2]]
+  array.sort((a, b) => a - b);
+  let pointer1 = 0;
+  let pointer2 = array.length - 1;
+  while (pointer1 < pointer2) {
+    if (array[pointer1] + array[pointer2] < targetSum) pointer1++;
+    else if (array[pointer1] + array[pointer2] < targetSum) pointer2--;
+    else return [array[pointer1], array[pointer2]];
   }
 
   return [];
 }
 
-console.log(twoNumberSum([3, 5, -4, 8, 11, 1, -1, 6], 10))
+console.log(twoNumberSum([3, 5, -4, 8, 11, 1, -1, 6], 10));
 export default twoNumberSum;
